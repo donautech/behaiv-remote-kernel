e050663fbb8ddce0483197240a66a7df2749b4ec
dmi3coder <dmi3coder@gmail.com>
2019-09-28 11:26:11 +0300

    formatting fixes

--- app/__init__.py
+++ app/__init__.py
@@ -13,13 +13,11 @@ from app.kernel import bp as kernel_bp
 from app.provider import bp as provider_bp
 from app.storage import bp as storage_bp
 
-
 app.register_blueprint(client_bp, url_prefix='/')
 app.register_blueprint(config_bp, url_prefix='/config')
 app.register_blueprint(kernel_bp, url_prefix='/kernel')
 app.register_blueprint(provider_bp, url_prefix='/provider')
 app.register_blueprint(storage_bp, url_prefix='/storage')
-
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
--- app/client/routes.py
+++ app/client/routes.py
@@ -1,5 +1,7 @@
-from app.client import bp
 from flask import redirect
+
+from app.client import bp
+
 
 @bp.route('/')
 def redirect_webapp():
