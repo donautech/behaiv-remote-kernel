fa844d62a35848c6d9bddea5035dc5ab4f42481f
dmi3coder <dmi3coder@gmail.com>
2019-10-05 11:56:43 +0300

    rolled back static routing

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
