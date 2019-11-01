90eea26b9960e1a88ea99c7fadf9a65eb6df95eb
dmi3coder <dmi3coder@gmail.com>
2019-11-01 11:19:58 +0200

    added docstring to client blueprint

--- app/client/routes.py
+++ app/client/routes.py
@@ -5,4 +5,13 @@ from app.client import bp
 
 @bp.route('/')
 def redirect_webapp():
+    '''
+    Redirects to static folder with react ui
+    ---
+    tags:
+        - ui
+    responses:
+        302:
+            description: redirection to /web/index.html
+    '''
     return redirect("/web/index.html", code=302)
