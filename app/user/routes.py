902991f626f59d417fffb1978a2b75bfeb106cad
dmi3coder <dmi3coder@gmail.com>
2019-11-02 09:25:51 +0200

    finished all client's endpoints for swagger

--- app/user/routes.py
+++ app/user/routes.py
@@ -12,6 +12,37 @@ from app.user import bp
 
 @bp.route('/', methods=['POST'])
 def register_user():
+    """
+    Register user/client in Behaiv
+    ---
+    tags:
+        - user
+    parameters:
+        - in: body
+          name: body
+          schema:
+            id: User
+            required:
+                - login
+            properties:
+                login:
+                    type: string
+                    description: login of user
+                token:
+                    type: string
+                    description: token that will be used for submitting data
+
+    responses:
+        200:
+            description: If network is ready to predict
+            content:
+                application/json:
+                    schema:
+                        $ref: '#/definitions/User'
+
+        500:
+            description: some problems occurred from backend side
+    """
     user_data = request.json
     new_user = User(login=user_data['login'], token=str(uuid.uuid1()), verification_token=str(uuid.uuid1()))
     # we allow users to set their own token, e.g if mobile developers decide to attach devices' uuid
